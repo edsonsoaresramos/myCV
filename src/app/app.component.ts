69de97c2d6fa0726e91250010d77cfccd452a569
Edson Soares <edson.soares.r@gmail.com>
2019-03-21 22:13:18 +0100

    initial testes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'mycv-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'myCV';
+  appTitle = 'myCV';
+  appMyCV = 'my app myCV';
 }
